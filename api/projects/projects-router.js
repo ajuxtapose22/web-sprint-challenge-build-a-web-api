25f78d3331d6956ddc52d8054dceeb74ee94c333
ajuxtapose22 <aaron.jardin@yahoo.com>
2024-09-19 13:55:06 -0700

    task one complete

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,10 @@
 // Write your "projects" router here!
+
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.send('Projects router working!!')
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const server = express();
 
-// Configure your server here
+
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
+const actionsRouter = require('./actions/actions-router')
+const projectsRouter = require('./projects/projects-router')
+
+// Configure your server here
+server.use(express.json())
+
+// Routes
+server.use('/api/actions', actionsRouter)
+server.use('/api/projects', projectsRouter)
+
+
+
 // Do NOT `server.listen()` inside this file!
+
+
+server.get('/', (req, res) => {
+    res.send('Server is up and running!')
+})
+
 
 module.exports = server;
